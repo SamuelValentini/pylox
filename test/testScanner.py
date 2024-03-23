cc65b445ac8ee6875df1078779731ff014c92200
Samuel <valentini.samuel@gmail.com>
2024-03-23 11:29:37 +0100

    Add test for slash and comments

--- test/testScanner.py
+++ test/testScanner.py
@@ -35,5 +35,22 @@ class TestScanner(unittest.TestCase):
             tokens = scanner.scanTokens()
             mock_error.assert_called_once_with(1, "@ Unexpected character.")
 
+    def test_slash(self):
+        expected = [TokenType.SLASH, TokenType.MINUS, TokenType.EOF]
+        scanner = Scanner("/ -")
+        tokens = scanner.scanTokens()
+        for token, expected in zip(tokens, expected):
+            self.assertEqual(token.type, expected)
+
+
+    def test_comment(self):
+        with patch("pylox.lox.error") as mock_error:
+            scanner = Scanner("//@ this is a comment")
+            tokens = scanner.scanTokens()
+            mock_error.assert_not_called
+
+        self.assertEqual(len(tokens), 1)
+        self.assertEqual(tokens[0].type, TokenType.EOF)
+
 if __name__ == '__main__':
     unittest.main()
