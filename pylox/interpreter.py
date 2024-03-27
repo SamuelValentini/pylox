e5347387467c92e61b3b04c64c3478d0a14d9a87
Samuel <valentini.samuel@gmail.com>
2024-03-27 23:27:11 +0100

    add runtime error handling

--- pylox/interpreter.py
+++ pylox/interpreter.py
@@ -1,10 +1,32 @@
-import Expr
-from visitor import Visitor
-from token import TokenType
-from token import Token
+import pylox.Expr
+from pylox.errorHandler import ErrorHandler
+from pylox.visitor import Visitor
+from pylox.token import TokenType
+from pylox.token import Token
 
 
 class Interpreter(Visitor):
+    def __init__(self, errorHandler):
+        self.errorHandler = errorHandler
+
+    def interpret(self, expression):
+        try:
+            value = self.evaluate(expression)
+            print(self.stringify(value))
+            return value
+        except RuntimeError as error:
+            self.errorHandler.runtimeError(error)
+
+    def stringify(self, object):
+        if object is None:
+            return "nil"
+        elif isinstance(object, float):
+            object = str(object)
+            if object.endswith(".0"):
+                return int(float(object))
+        else:
+            return str(object)
+
     def evaluate(self, expression):
         return expression.accept(self)
 
@@ -13,12 +35,19 @@ class Interpreter(Visitor):
             return False
         return bool(value)
 
-    def isEqual(a, b):
+    def isEqual(self, a, b):
         if a is None and b is None:
             return True
         if a is None:
             return False
         return a == b
+
+    def checkNumberOperand(self, operator, operand, other=1.0):
+        # Hacky way! In this way you can use the same function
+        # to check for both binary and unary operators
+        if isinstance(operand, float) and isinstance(other, float):
+            return
+        raise RuntimeError(operator, "Operand must be a number")
 
     def visitBinaryExpr(self, expression):
         left = self.evaluate(expression.left)
@@ -29,20 +58,30 @@ class Interpreter(Visitor):
                     isinstance(left, str) and isinstance(right, str)
                 ):
                     return left + right
+                raise RuntimeError(
+                    expression.operator, "Operands must be two numbers or two strings."
+                )
 
             case TokenType.MINUS:
+                self.checkNumberOperand(expression.operator, left, right)
                 return float(left) - float(right)
             case TokenType.SLASH:
+                self.checkNumberOperand(expression.operator, left, right)
                 return float(left) / float(right)
             case TokenType.STAR:
+                self.checkNumberOperand(expression.operator, left, right)
                 return float(left) * float(right)
             case TokenType.GREATER:
+                self.checkNumberOperand(expression.operator, left, right)
                 return float(left) > float(right)
             case TokenType.GREATER_EQUAL:
+                self.checkNumberOperand(expression.operator, left, right)
                 return float(left) >= float(right)
             case TokenType.LESS:
+                self.checkNumberOperand(expression.operator, left, right)
                 return float(left) < float(right)
             case TokenType.LESS_EQUAL:
+                self.checkNumberOperand(expression.operator, left, right)
                 return float(left) <= float(right)
             case TokenType.BANG_EQUAL:
                 return not self.isEqual(left, right)
@@ -52,7 +91,7 @@ class Interpreter(Visitor):
         return None
 
     def visitGroupingExpr(self, expression):
-        return self.evaluate(expression)
+        return self.evaluate(expression.expr)
 
     def visitLiteralExpr(self, expression):
         return expression.value
@@ -63,6 +102,7 @@ class Interpreter(Visitor):
             case TokenType.BANG:
                 return not self.isTruthy(right)
             case TokenType.MINUS:
+                self.checkNumberOperand(expression.operator, right)
                 return -float(right)
 
         return None
@@ -70,12 +110,20 @@ class Interpreter(Visitor):
 
 if __name__ == "__main__":
     expression = Expr.Binary(
-        Expr.Literal(1),
+        Expr.Literal(1.0),
         Token(TokenType.PLUS, "+", None, 1),
         Expr.Binary(
-            Expr.Literal(5), Token(TokenType.SLASH, "/", None, 1), Expr.Literal(2)
+            Expr.Literal(5.0), Token(TokenType.SLASH, "/", None, 1), Expr.Literal(2.0)
         ),
     )
+
+    expression = Expr.Binary(
+        Expr.Literal("HELLO"),
+        Token(TokenType.GREATER, "-", None, 1),
+        Expr.Literal("WORLD"),
+    )
+
+    expression = Expr.Unary(Token(TokenType.MINUS, "-", None, 1), Expr.Literal("HELLO"))
 
     printer = Interpreter()
     print(printer.evaluate(expression))
