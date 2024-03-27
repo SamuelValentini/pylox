0fde4f5f45e57246fb3012d9b17e46838baa6ac5
Samuel <valentini.samuel@gmail.com>
2024-03-27 23:25:10 +0100

    Implement error handler to get rid of the global var

--- pylox/errorHandler.py
+++ pylox/errorHandler.py
@@ -1,6 +1,7 @@
 class ErrorHandler:
     def __init__(self):
         self.hadError = False
+        self.hadRuntimeError = False
 
     def error(self, line, message):
         self.report(line, "", message)
@@ -8,3 +9,8 @@ class ErrorHandler:
     def report(self, line, where, message):
         print(f"line: {line}] Error {where}: {message}")
         self.hadError = True
+
+    def runtimeError(self, error):
+        operator, message = error.args
+        print(f"{message} \n[line {operator.line}]")
+        self.hadRuntimeError = True
