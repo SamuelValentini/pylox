f3df118d275a46ca5b4bc0381b8ed6d964efbdd5
Samuel <valentini.samuel@gmail.com>
2024-04-07 17:18:09 +0200

    Add scoping feature to environment

--- pylox/environment.py
+++ pylox/environment.py
@@ -1,12 +1,26 @@
 class Environment:
-    def __init__(self):
+    def __init__(self, enclosing=None):
         self.values = {}
+        self.enclosing = enclosing
 
     def define(self, name, value):
         self.values[name] = value
 
     def get(self, name):
-        try:
+        if name.lexeme in self.values:
             return self.values[name.lexeme]
-        except KeyError:
-            raise RuntimeError(name, f"Undefined variable {name.lexeme}.")
+        if self.enclosing is not None:
+            return self.enclosing.get(name)
+
+        raise RuntimeError(name, f"Undefined variable {name.lexeme}.")
+
+    def assign(self, name, value):
+        if name.lexeme in self.values:
+            self.values[name.lexeme] = value
+            return None
+
+        if self.enclosing is not None:
+            self.enclosing.assign(name, value)
+            return None
+
+        raise RuntimeError(name, f"Undefined variable {name.lexeme}.")
